17da3c110500f17f43eb77b7624a38c75d504199
charlie <charliehuang09@gmail.com>
2022-06-16 20:50:44 -0700

    checkpoint

--- Subarray Sums I.cpp
+++ Subarray Sums I.cpp
@@ -3,29 +3,29 @@ using namespace std;
 int main(){
     int length, target;
     cin >> length >> target;
-    int input[length];
-    int t;
-    int p = 0;
-    int output = 0;
+    int prefix[length];
+    int psum = 0, t;
     for (int i = 0; i < length; i++) {
         cin >> t;
-        input[i] = t;
-        int l = i;
-        int s = 0;
-        while (l >= 0){
-            s += input[l];
-            if (s == target){
-                output++;
-                break;
-            }
-            else if (s < target){
-                l--;
-            }
-            else {
-                break;
-            }
+        psum += t;
+        prefix[i] = psum;
+    }
+
+    int l = 0, h = 0, output = 0;
+    while (l < length - 1 && h < length){
+        if (prefix[h] - prefix[l] == target){
+            output++;
+            l++;
+            h++;
+        }
+        else if (prefix[h] - prefix[l] < target){
+            h++;
+        }
+        else{
+            l++;
         }
     }
-    cout << output;
+    cout << output << endl;
+
+    return 0;
 }
-//2 6 7 9 16
