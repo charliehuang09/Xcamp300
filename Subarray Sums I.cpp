2d1fb0097d47e80c2f031ae28bc97ea73fa24e7d
charlie <charliehuang09@gmail.com>
2022-06-16 20:54:27 -0700

    100pts

--- Subarray Sums I.cpp
+++ Subarray Sums I.cpp
@@ -1,31 +1,33 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int length, target;
-    cin >> length >> target;
-    int prefix[length];
-    int psum = 0, t;
-    for (int i = 0; i < length; i++) {
-        cin >> t;
-        psum += t;
-        prefix[i] = psum;
-    }
-
-    int l = 0, h = 0, output = 0;
-    while (l < length - 1 && h < length){
-        if (prefix[h] - prefix[l] == target){
-            output++;
-            l++;
-            h++;
-        }
-        else if (prefix[h] - prefix[l] < target){
-            h++;
-        }
-        else{
-            l++;
-        }
-    }
-    cout << output << endl;
-
-    return 0;
-}
+//#include <iostream>
+//using namespace std;
+//int main(){
+//    int length, target;
+//    cin >> length >> target;
+//    length++;
+//    int prefix[length];
+//    prefix[0] = 0;
+//    int psum = 0, t;
+//    for (int i = 1; i < length; i++) {
+//        cin >> t;
+//        psum += t;
+//        prefix[i] = psum;
+//    }
+//
+//    int l = 0, h = 0, output = 0;
+//    while (l < length - 1 && h < length){
+//        if (prefix[h] - prefix[l] == target){
+//            output++;
+//            l++;
+//            h++;
+//        }
+//        else if (prefix[h] - prefix[l] < target){
+//            h++;
+//        }
+//        else{
+//            l++;
+//        }
+//    }
+//    cout << output << endl;
+//
+//    return 0;
+//}
