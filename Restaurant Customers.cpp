a38af5e0a9d19707c482c4d684bf255c6f5c8ab7
charlie <charliehuang09@gmail.com>
2022-06-20 12:09:45 -0700

    100pts

---
+++ Restaurant Customers.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//#include<cstring>
+//#include<map>
+//
+//using namespace std;
+//int main(){
+//    int length;
+//    cin >> length;
+//    int a, b;
+//    map<int, int> input;
+//
+//    for (int i = 0; i < length; i++) {
+//        cin >> a >> b;
+//        input[a] += 1;
+//        input[b] += -1;
+//    }
+//
+//    int output = 0;
+//    int current = 0;
+//    for (auto itr : input){
+//        current += itr.second;
+//        output = max(output, current);
+//    }
+//
+//    cout << output << endl;
+//
+//    return 0;
+//
+//
+//
+//}
