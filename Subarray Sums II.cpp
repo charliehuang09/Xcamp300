8fc2ef3611abdbcf6a666f152c80132f976f8a56
charlie <charliehuang09@gmail.com>
2022-06-22 20:33:41 -0700

    92pts

--- Subarray Sums II.cpp
+++ Subarray Sums II.cpp
@@ -14,17 +14,22 @@ int main(){
         psum[i] = psum[i - 1] + t;
     }
 
+//    map[0] = 1;
+
     int output = 0;
     for (int i = 1; i < length; i++){
-        map[psum[i]] = - (target - psum[i]);
+//        cout << - (target - psum[i]) << "\n";
+        map[psum[i]]++;
         if (map.find(- (target - psum[i])) != map.end()){
+            output += map[- (target - psum[i])];
+        }
+
+        if (psum[i] == target && target != 0){
             output++;
         }
-        if (psum[i] == target){
-            output++;
-        }
+
     }
-    cout << output;
+    cout << "\n" << output;
 
     return 0;
 }
