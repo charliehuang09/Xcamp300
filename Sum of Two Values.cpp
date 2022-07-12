b9bda4c57fa026e52c45330f1104d3e9a755c0cd
charlie <charliehuang09@gmail.com>
2022-07-12 13:04:54 -0700

    Did not finish Sum of three values and Sum of two values

--- Sum of Two Values.cpp
+++ Sum of Two Values.cpp
@@ -1,20 +1,52 @@
-//#include <iostream>
-//#include <map>
-//using namespace std;
-//
-//int main(){
-//    long long int length, target, t;
-//    cin >> length >> target;
-//    map<long long int, long long int> map;
-//
-//    for (long long int i = 0; i < length; i++) {
-//        cin >> t;
-//        if (map[t] != 0){
-//            cout << map[t] << " " << i + 1;
-//            return 0;
-//        }
-//        map[target - t] = i + 1;
-//    }
-//
-//    cout << "IMPOSSIBLE";
-//}
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct place{
+    long long int val;
+    long long int i;
+};
+
+bool compare(place a, place b){
+    return a.val < b.val;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    long long int length, target;
+    cin >> length >> target;
+
+    place input[length];
+    for (long long int i = 0;i < length; i++){
+        cin >> input[i].val;
+        input[i].i = i;
+    }
+
+    sort(input, input + length, compare);
+
+    long long int l = 0, h = length - 1;
+    while (l != h){
+        if (input[h].val + input[l].val == target && l != h){
+            cout << input[l].i + 1 << " " << input[h].i + 1;
+            return 0;
+        }
+        else if (input[h].val + input[l].val > target){
+            h--;
+        }
+        else{
+            l++;
+        }
+    }
+    while(1){
+        cout << "IMPOSSIBLE";
+    }
+    cout << "IMPOSSIBLE";
+    return 0;
+}
+/*
+4 8
+2 7 5 1
+
+0 2 3
+*/
--- Sum of three values.cpp
+++ Sum of three values.cpp
@@ -1,43 +1,35 @@
-#include <iostream>
-
-using namespace std;
-int main(){
-    int length, target;
-    cin >> length >> target;
-
-    int input[length];
-    for (int i = 0; i < length; i++){
-        cin  >> input[i];
-    }
-
-    int output;
-
-    for (int i = 0; i < length - 2; i++){
-        int l = i + 1;
-        int h = length - 1;
-//        cout  << i << " " << l << " " << h << "\n";
-
-        while (l != h){
-            if (input[l] + input[h] + input[i] == target){
-                cout << i + 1 << " " << l + 1 << " " << h + 1;
-                return 0;
-            }
-
-            if (input[l] + input[h] + input[i] > 0){
-                l++;
-            }
-            else{
-                h--;
-            }
-        }
-    }
-
-    cout << "IMPOSSIBLE";
-
-}
-/*
-4 8
-2 7 5 1
-
-0 2 3
-*/
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//struct place{
+//    int val;
+//    int i;
+//};
+//
+//bool compare(place a, place b){
+//    return a.val < b.val;
+//}
+//
+//int main(){
+//    int length, target;
+//    cin >> length >> target;
+//
+//    place input[length];
+//    for (int i = 0;i < length; i++){
+//        cin >> input[i].val;
+//        input[i].i = i;
+//    }
+//
+//    sort(input, input + length, compare);
+//
+//    for (int i )
+//
+//    return 0;
+//}
+///*
+//4 8
+//2 7 5 1
+//
+//0 2 3
+//*/
