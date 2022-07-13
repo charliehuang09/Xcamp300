d0960caef779a0c2b65cf492dd74d6fb66c1ece1
charlie <charliehuang09@gmail.com>
2022-07-13 09:26:14 -0700

    Clean up code

---
+++ Remove duplication and sort.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//int main(){
+//    int length;
+//    cin >> length;
+//    int input[length];
+//    for (int i = 0; i < length; i++) {
+//        cin >> input[i];
+//    }
+//    sort(input, input + length);
+//
+//    cout << input[0] << " ";
+//    for (int i = 1; i < length; i++) {
+//        if (input[i - 1] != input[i]){
+//            cout << input[i] << " ";
+//        }
+//    }
+//}
