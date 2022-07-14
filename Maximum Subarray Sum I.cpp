600cbe3d70af57ac3ca3371c53efe1bc34ce93e2
charlie <charliehuang09@gmail.com>
2022-07-13 17:41:11 -0700

    Maximum Subarray Sum I

---
+++ Maximum Subarray Sum I.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int length;
+    long long int output = -2147483648;
+    cin >> length;
+    long long int input[length];
+    for (int i = 0; i < length; i++) {
+        cin >> input[i];
+        output = max(input[i], output);
+    }
+
+    long long int sum = 0;
+    for (long long int i = 0; i < length; i++) {
+        if (sum + input[i] < 0){
+            sum = 0;
+        }
+        else{
+            sum += input[i];
+            output = max(sum, output);
+        }
+    }
+
+    cout << "\n" << output;
+
+
+}
