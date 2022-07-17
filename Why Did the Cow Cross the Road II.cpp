673b8924c729b2eefe3056361d0d7567bc79e6a5
charlie <charliehuang09@gmail.com>
2022-07-17 16:59:07 -0700

    90pts

---
+++ Why Did the Cow Cross the Road II.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    int lights, inputs, k;
+    cin >> lights >> k >> inputs;
+//    k--;//CHANGED
+    int input[lights];
+    memset(input, 0, sizeof(input));
+
+    int t, sum = 0;
+    for (int i = 0; i < inputs; i++){
+        cin >> t;
+        input[t] = 1;
+    }
+
+//    cout << "\n";
+//    for (int i = 0; i < lights; i++){
+//        cout << input[i] << " ";
+//    }
+//    cout << "\n\n";
+
+    for (int i = 0; i <= k; i++){
+        sum += input[i];
+    }
+
+//    cout << sum;
+
+    int output = 2147483647;
+    output = min(output, sum);
+    for (int i = k + 1; i < lights; i++){
+        sum -= input[i - k];
+        sum += input[i];
+//        cout << sum << "\n";
+        output = min(output, sum);
+    }
+
+    cout << "\n" << output;
+
+}
