20145c6437d3f36a0642a2cd82132840d4bc9b85
charlie <charliehuang09@gmail.com>
2022-07-30 21:11:13 -0700

    Cutting Wood

--- Cutting Wood.cpp
+++ Cutting Wood.cpp
@@ -7,40 +7,49 @@
 //#include <algorithm>
 //using namespace std;
 //#define ll long long
-//int wood(ll input, ll wood[], ll length){
+//ll input[10000000];
+//int length, target;
+//ll get_wood(int value){
 //    ll output = 0;
 //    for (int i = 0; i < length; i++){
-//        if (wood[i] - input > 0){
-//            output += wood[i] - input;
-//        }
+//        if (input[i] - value > 0) output += input[i] - value;
 //    }
 //    return output;
 //}
 //
-//
 //int main(){
 //    ios_base::sync_with_stdio(0);
 //    cin.tie(0);
-//
-//    ll length, target;
 //    cin >> length >> target;
-//    ll input[length];
 //    for (int i = 0; i < length; i++){
 //        cin >> input[i];
 //    }
 //    sort(input, input + length);
-//    ll l = 0;
-//    ll h = 2147483;
-//    while (h - l > 1) {
-//        int mid = (h + l) / 2;
-//        if (wood(mid, input, length) > target) {
-//            l = mid + 1;
+////    cout << get_wood(36);
+//    int l = 0;
+//    int r = input[length - 1];
+//    while(r - l > 1){
+//        int mid = (r + l) / 2;
+//        ll value = 0;
+//        for (int i = 0; i < length; i++){
+//            if (input[i] - mid > 0) value += input[i] - mid;
 //        }
-//        else {
-//            h = mid;
+//        if (value < target){
+//            r = mid;
+//        }
+//        else{
+//            l = mid;
 //        }
 //    }
-////    l--;
-//    while(wood(l + 1, input, length) >= target)l++;
+////    cout << r << " " << l;
+//    while(get_wood(l + 1) >= target){
+//        l++;
+//    }
+//    cout << "\n";
 //    cout << l;
 //}
+///*
+//5 20
+//4 26 40 42 46
+// */
+////max
